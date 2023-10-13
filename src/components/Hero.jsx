1c0d572c5dab81d13c25d099800e51142c4c797f
hoperawr <hopeleong@gmail.com>
2023-10-13 17:25:02 +0800

    AI Summarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("twitter.com")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">Simplify bla bla bla</h2>
+    </header>
+  );
+};
+
+export default Hero;
